b2c162602dee074adf739edb2fc81dc5db093e34
Nichelle Barton <52866856+nbarton76@users.noreply.github.com>
2023-09-02 21:13:32 -0500

    removed unused frameworks

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,16 +1,13 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.Size;
-import java.util.Objects;
 
 /**
  * Created by Chris Bay
  */
 @Entity
-public class EventCategory extends AbstractEntity{
+public class EventCategory extends AbstractEntity {
 
 
     @Size(min=3, message="Name must be at least 3 characters long")
